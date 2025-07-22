d6a5f3c5f99b81909e3eafb8beafdc5fc039a3ab
Hassan <hassanabangura1@gmail.com>
2025-07-22 13:20:26 +0000

    added netlify link to qrcodepage

--- src/pages/QRCodePage.jsx
+++ src/pages/QRCodePage.jsx
@@ -5,7 +5,7 @@ const QRCodePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h2 className="text-xl font-bold mb-4">Scan to View Our Menu</h2>
-      <QRCodeSVG value="http://localhost:3000/view-menu" size={200} />
+      <QRCodeSVG value="thingsandsomethingmenupage.netlify.app" size={200} />
     </div>
   );
 };
