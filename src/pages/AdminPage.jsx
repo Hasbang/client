eb48e64af1f71caecbf681cf5e746a89c437429e
Hassan <hassanabangura1@gmail.com>
2025-07-24 08:56:11 +0000

    added backend links

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -41,7 +41,7 @@ const AdminPage = () => {
 
 
   try {
-    await axios.post('http://localhost:3000/menu', data, {
+    await axios.post('http://resmenubackend.onrender.com', data, {
       headers: { 'Content-Type': 'multipart/form-data' }
     });
     alert('Item added!');
--- src/pages/CustomerMenuPage.jsx
+++ src/pages/CustomerMenuPage.jsx
@@ -9,7 +9,7 @@ const CustomerMenuPage = () => {
 
  
     useEffect(() => {
-    axios.get('http://localhost:3000/menu')
+    axios.get('http://resmenubackend.onrender.com')
       .then(res => {
         setItems(res.data);
         setFilteredItems(res.data);
