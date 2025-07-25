058d4cd4bd950d671657982f59b3eb76c73fc00a
Hassan <hassanabangura1@gmail.com>
2025-07-25 10:51:38 +0000

    redirecting qrcode to customer page

--- src/pages/QRCodePage.jsx
+++ src/pages/QRCodePage.jsx
@@ -5,7 +5,7 @@ const QRCodePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h2 className="text-xl font-bold mb-4">Scan to View Our Menu</h2>
-      <QRCodeSVG value="thingsandsomethingmenupage.netlify.app" size={200} />
+      <QRCodeSVG value="thingsandsomethingmenupage.netlify.app/view-menu" size={200} />
     </div>
   );
 };
